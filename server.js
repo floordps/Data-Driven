f5e94ea062da0038ccefbedadbc93faf3d8e95bc
jakeowns <beanshield@gmail.com>
2015-02-12 12:11:51 -0800

    Add express setup.

---
+++ server.js
@@ -0,0 +1,15 @@
+var express = require('express');
+var app = express();
+var server = app.listen(80);
+var bodyParser = require('body-parser');
+
+app.set('views', __dirname + '/views');
+app.set('view engine', 'jade');
+app.locals.pretty = true;
+app.locals.basedir = __dirname;
+app.use(bodyParser.json());
+app.use(bodyParser.urlencoded({ extended: true }));
+
+app.get('/', function(req, res, next) {
+  res.render('index');
+});
