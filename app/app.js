db56f215b40bceeccb1eda9486817a2557e3c101
Yanrong Huang <yanhuang1988@gmail.com>
2015-02-13 12:52:51 -0800

    Create angular app and setup route

---
+++ app/app.js
@@ -0,0 +1,16 @@
+var app = angular.module('data-driven', ['ngRoute']);
+
+app.config(['$routeProvider', function($routeProvider) {
+  $routeProvider
+    .when('/', {
+      templateUrl : 'partials/index.jade',
+      controller : 'clientCtrl'
+    })
+    .when('/master', {
+      templateUrl : 'partials/master.jade',
+      controller : 'masterCtrl'
+    })
+    .otherwise({
+      redirectTo: '/'
+    });
+}]);
