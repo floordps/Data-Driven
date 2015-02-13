889e1f2779a872ad83bad4727f4e9d122c703023
jakeowns <beanshield@gmail.com>
2015-02-12 17:53:01 -0800

    Add master.jade file.

--- app/js/app.js
+++ app/js/app.js
@@ -1,13 +1,19 @@
 Reveal.initialize({
   loop: true,
   controls: true,
-  autoSlide: 3000,
   keyboard: true,
   width: 960,
   height: 700,
   margin: 0.1,
   progress: false,
+  multiplex: {
+    secret: null,
+    id: '1',
+    url: '127.0.0.1:5000'
+  },
   dependencies: [
+    { src: '/socket.io/socket.io.js', async: true },
+    { src: '/app/plugin/multiplex/client.js', async: true },
     { src: 'lib/js/classList.js', condition: function() { return !document.body.classList; } },
     { src: '/app/plugin/markdown/marked.js', condition: function() { return !!document.querySelector( '[data-markdown]' ); } },
     { src: '/app/plugin/markdown/markdown.js', condition: function() { return !!document.querySelector( '[data-markdown]' ); } }
