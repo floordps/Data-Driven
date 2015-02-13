52b7c285e806baa50012794de84bb9aaf7fd7a8d
jakeowns <beanshield@gmail.com>
2015-02-12 17:54:52 -0800

    Add socket.io server.

--- server.js
+++ server.js
@@ -1,6 +1,7 @@
 var express = require('express');
 var app = express();
 var server = app.listen(process.env.PORT || 5000);
+var io = require('socket.io')(server);
 var bodyParser = require('body-parser');
 
 app.set('views', __dirname + '/views');
@@ -13,4 +14,12 @@ app.use(bodyParser.urlencoded({ extended: true }));
 app.get('/', function(req, res, next) {
   res.render('index');
 });
+app.get('/master', function(req, res, next) {
+  res.render('master');
+});
 app.use('/app', express.static(__dirname + '/app'));
+io.sockets.on('connection', function(socket) {
+  socket.on('slidechanged', function(data) {
+    socket.broadcast.emit(data.socketId, data);
+  });
+});
