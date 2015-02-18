1455dcdd2a0270e80b681a27976135775468aa46
Yanrong Huang <yanhuang1988@gmail.com>
2015-02-17 22:09:32 -0800

    Add additional buttons to bootstrap-markdown editor

---
+++ app/js/bootstrap-markdown.buttons.js
@@ -0,0 +1,43 @@
+$(function() {
+  $('#text-editor').markdown({
+    iconlibrary: 'fa',
+    hiddenButtons: 'cmdPreview',
+    disabledButtons: 'cmdPreview',
+    additionalButtons: [
+      [{
+        name: 'slideButton',
+        data: [{
+          name: 'addSlide',
+          toggle: false,
+          title: 'Add Slide',
+          icon: 'fa fa-plus',
+          callback: function(e) {
+            var end = e.getContent().length;
+            e.setSelection(end, end);
+            e.replaceSelection('\n---\n');
+          }
+        }]
+      }, {
+        name: 'graphButton',
+        data: [{
+          name: 'insertGraph',
+          title: 'Graphs',
+          toggle: false,
+          icon: 'fa fa-line-chart',
+          callback: function(e) {
+          }
+        }]
+      }, {
+        name: 'tableButton',
+        data: [{
+          name: 'insertTable',
+          title: 'Table',
+          toggle: false,
+          icon: 'fa fa-table',
+          callback: function(e) {
+          }
+        }]
+      }]
+    ]
+  });
+});
