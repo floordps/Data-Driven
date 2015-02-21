cd5705374b75b961d3685f2e9883ffb6d2a08f34
jakeowns <beanshield@gmail.com>
2015-02-20 21:54:32 -0800

    Add jsforce report route to return report dataCells.

--- server.js
+++ server.js
@@ -3,6 +3,7 @@ var app = express();
 var server = app.listen(process.env.PORT || 5000);
 var io = require('socket.io')(server);
 var bodyParser = require('body-parser');
+var jsforce = require('jsforce');
 
 app.set('views', __dirname + '/views');
 app.set('view engine', 'jade');
@@ -20,6 +21,25 @@ app.get('/partials/:id', function(req, res, next) {
 /*app.get('/master', function(req, res, next) {
   res.render('master');
 });*/
+var js = require('./jsConnect.json');
+app.post('/report/:id', function(req, res, next) {
+  var conn = new jsforce.Connection({
+    oauth2: {
+      clientId: js.clientId,
+      clientSecret: js.clientSecret,
+      redirectUri: js.redirectUri
+    }
+  });
+  conn.login(js.username, js.password, function(err, user) {
+    if(err) return res.status(500).send(err);
+    conn.analytics.reports(function(err, reports) {
+      var id = req.params.id;
+      conn.analytics.report(id).execute({ details: true }, function(err, result) {
+        res.json(result);
+      });
+    });
+  });
+});
 
 app.use('/app', express.static(__dirname + '/app'));
 app.use('/bower', express.static(__dirname + '/bower_components'));
