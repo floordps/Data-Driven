cd9402cef7a81fdb8eb0ba74f6aad3f0f8717a13
floordps <yanhuang1988@gmail.com>
2015-02-26 23:19:36 -0800

    Create customer directive: chat, audience and master

---
+++ app/directive/directives.js
@@ -0,0 +1,54 @@
+app.directive('chat', function() {
+  return {
+    restrict: 'E',
+    templateUrl: 'partials/chat.jade'
+  };
+});
+
+app.directive('audience', function($compile, $http) {
+  return {
+    restrict: 'E',
+    templateUrl: 'partials/audienceSlide.jade',
+    link: function(scope, elem, attr) {
+      Reveal.addEventListener('ready', function(event) {
+        scope.graph = {};
+        $compile($('.graph'))(scope);
+        $('.graph').each(function() {
+          var id = $(this).attr('reportId');
+          $http.post('/report/'+id).success(function(data) {
+            var arr = data.factMap['T!T'].rows;
+            var obj = [{
+              key: id,
+              values: arr.map(function(val, i) { return [ i, +new Date(val.dataCells[6].value) ]; })
+            }];
+            scope.graph[id] = obj;
+          });
+        });
+      });
+    }
+  };
+});
+
+app.directive('master', function($compile, $http) {
+  return {
+    restrict: 'E',
+    templateUrl: 'partials/masterSlide.jade',
+    link: function(scope, elem, attr) {
+      Reveal.addEventListener('ready', function(event) {
+        scope.graph = {};
+        $compile($('.graph'))(scope);
+        $('.graph').each(function() {
+          var id = $(this).attr('reportId');
+          $http.post('/report/'+id).success(function(data) {
+            var arr = data.factMap['T!T'].rows;
+            var obj = [{
+              key: id,
+              values: arr.map(function(val, i) { return [ i, +new Date(val.dataCells[6].value) ]; })
+            }];
+            scope.graph[id] = obj;
+          });
+        });
+      });
+    }
+  };
+});
