377386fe5f76cd6b4820bd89b7dc1939fddbcf87
floordps <yanhuang1988@gmail.com>
2015-02-26 23:18:25 -0800

    Setup Chat.io

--- app/controller/controllers.js
+++ app/controller/controllers.js
@@ -1,19 +1,28 @@
-app.controller('clientCtrl', function($scope, $timeout, $compile, $http) {
-  Reveal.addEventListener('ready', function(event) {
-    $scope.graph = {};
-    $compile($('.graph'))($scope);
-    $('.graph').each(function() {
-      var id = $(this).attr('reportId');
-      $http.post('/report/'+id).success(function(data) {
-        var arr = data.factMap['T!T'].rows;
-        var obj = [{
-          key: id,
-          values: arr.map(function(val, i) { return [ i, +new Date(val.dataCells[6].value) ]; })
-        }];
-        $scope.graph[id] = obj;
-      });
-    });
+app.controller('clientCtrl', function($scope, socket) {
+  $scope.messages = [];
+  socket.on('init', function (data) {
+    $scope.name = data.name;
   });
+
+  socket.on('send:message', function (message) {
+    $scope.messages.push({
+      user: message.user,
+      text: message.text,
+      });
+  });
+
+  $scope.sendMessage = function () {
+    socket.emit('send:message', {
+      message: $scope.message
+    });
+
+    $scope.messages.push({
+      user: $scope.name,
+      text: $scope.message
+    });
+
+    $scope.message = '';
+  };
 });
 
 app.controller('editorCtrl', function($scope) {
