adeb15ef47943bd3364bca36aac2f2ce99f0cd41
jakeowns <beanshield@gmail.com>
2015-02-26 22:19:39 -0800

    Add slideShow schema.

---
+++ models/slideShow.js
@@ -0,0 +1,7 @@
+var mongoose = require('mongoose');
+var slideShowSchema = new mongoose.Schema({
+  //options: String,
+  id: String,
+  slides: String
+}, { collection: 'slideShows' } );
+module.exports = mongoose.model('SlideShow', slideShowSchema, 'slideShows');
