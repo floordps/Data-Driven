5e673c4388724692fe46948007663fa3d97f36a2
jakeowns <beanshield@gmail.com>
2015-03-10 17:30:03 -0700

    Update options object. Add createGraph() to generate graph markdown.

--- app/controller/controllers.js
+++ app/controller/controllers.js
@@ -27,6 +27,7 @@ app.controller('clientCtrl', function($rootScope, $scope, socket) {
 
 app.controller('editorCtrl', function($scope, $http) {
   $scope.reportDetails = true;
+  $scope.graph = {};
   $scope.showGraphType = function(e) {
     if (e == 'rID') {
       $scope.reportDetails = false;
@@ -36,35 +37,35 @@ app.controller('editorCtrl', function($scope, $http) {
   };
 
   $scope.options = [
-    {option: 'X Axis Data Format'},
-    {option: 'Y Axis Date Format'},
-    {option: 'Show X Axis'},
-    {option: 'Show Y Axis'},
-    {option: 'Is Area'},
-    {option: 'Interactive'},
-    {option: 'Interactive Guide Line'},
-    {option: 'Tooltips'},
-    {option: 'Clip Edge'},
-    {option: 'Clip Voronoi'}
+    {name: 'xAxisFmt', option: 'X Axis Data Format'},
+    {name: 'yAxisFmt', option: 'Y Axis Date Format'},
+    {name: 'showYAxis', option: 'Show X Axis'},
+    {name: 'showXAxis', option: 'Show Y Axis'},
+    {name: 'isArea', option: 'Is Area'},
+    {name: 'interactive', option: 'Interactive'},
+    {name: 'guideLine', option: 'Interactive Guide Line'},
+    {name: 'tooltips', option: 'Tooltips'},
+    {name: 'clipEdge', option: 'Clip Edge'},
+    {name: 'clipVoronoi', option: 'Clip Voronoi'}
   ];
 
   $scope.gTypes = [
-    {type: 'Line Charts'},
-    {type: 'Stacked Area Charts'},
-    {type: 'Multi Bar Charts'},
-    {type: 'Multi Bar Horizontal Charts'},
-    {type: 'Discrete Bar Charts'},
-    {type: 'Pie Charts'},
-    {type: 'Scatter Charts'},
-    {type: 'Sparkline  Charts'},
-    {type: 'Cumulative Line Charts'},
-    {type: 'Line with Focus Charts'}
+    {name: 'line-chart', type: 'Line Charts'},
+    {name: 'stacked-area-chart', type: 'Stacked Area Charts'},
+    {name: 'multi-bar-chart', type: 'Multi Bar Charts'},
+    {name: 'multi-bar-horizontal-chart', type: 'Multi Bar Horizontal Charts'},
+    {name: 'discrete-bar-chart', type: 'Discrete Bar Charts'},
+    {name: 'pie-chart', type: 'Pie Charts'},
+    {name: 'scatter-chart', type: 'Scatter Charts'},
+    {name: 'sparkline-chart', type: 'Sparkline  Charts'},
+    {name: 'cumulative-line-chart', type: 'Cumulative Line Charts'},
+    {name: 'line-with-focus-chart', type: 'Line with Focus Charts'}
   ];
 
   $scope.labels = [];
 
   $scope.checkReport = function () {
-    $http.post('/report/' + $scope.reportId + '/desc').success(function(data) {
+    $http.post('/report/' + $scope.graph.reportId + '/desc').success(function(data) {
       if(data) {
         data.cols.forEach(function(data) {
         $scope.labels.push(data.label);
@@ -73,6 +74,16 @@ app.controller('editorCtrl', function($scope, $http) {
         $('#graphModal .modal-body').append('check err');
       }
     });
+  };
+
+  $scope.createGraph = function() {
+    var str = '&nbsp;<!-- .element: class="graph center-block" '
+      + 'nvd3-' + $scope.graph.graphType + '="x" '
+      + 'data="graph[\''+ $scope.graph.reportId +'\']"';
+    for(var o in $scope.graph.options) {
+      str += ' ' + o + '="'+ $scope.graph.options[o] +'"';
+    }
+    str += ' -->';
   };
 });
 
