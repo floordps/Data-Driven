d0ce5cd8bca450d823395fc5cf1902963ef2d007
jakeowns <beanshield@gmail.com>
2015-03-25 22:22:23 -0700

    Add view/all route.

--- routes/view.js
+++ routes/view.js
@@ -1,4 +1,4 @@
 var express = require('express');
 
 module.exports.Router = function(SlideShow) {
-  return express.Router()    .get('/:username', function(req, res, next) {      SlideShow.find({ author: req.params.username }, function(err, slideShows) {        res.json(slideShows);      });    })    .get('/:username/:slidename', function(req, res, next) {      SlideShow.findOne({        author: req.params.username,        slideName: req.params.slidename      }, function(err, slideShow) {        res.json(slideShow);      });    });};
+  return express.Router()    .get('/all', function(req, res, next) {      SlideShow.find({}, function(err, slideShows) {        res.json(slideShows);      });    })    .get('/:username', function(req, res, next) {      SlideShow.find({ author: req.params.username }, function(err, slideShows) {        res.json(slideShows);      });    })    .get('/:username/:slidename', function(req, res, next) {      SlideShow.findOne({        author: req.params.username,        slideName: req.params.slidename      }, function(err, slideShow) {        res.json(slideShow);      });    });};
